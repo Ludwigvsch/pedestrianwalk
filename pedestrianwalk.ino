59bc894eb9cbfe0c6db01167ee303f519733f4ad
Ludwigvsch <lvschoenfeldt@gmx.de>
2023-06-02 09:26:18 -0700

    init

---
+++ pedestrianwalk.ino
@@ -0,0 +1,47 @@
+int red = 10;
+int yellow = 9;
+int green = 8;
+int button = 12; // switch is on pin 12
+bool changing = false; // flag to indicate when lights are changing
+
+void setup(){
+    pinMode(red, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(green, OUTPUT);
+    pinMode(button, INPUT);
+    digitalWrite(green, HIGH);
+}
+
+void loop() {
+    if (!changing && digitalRead(button) == HIGH){
+        delay(150); // software debounce
+        if (digitalRead(button) == HIGH) {
+            // if the switch is HIGH, ie. pushed down - change the lights!
+            changing = true;
+            changeLights();
+            changing = false;
+        }
+    }
+}
+
+void changeLights(){
+    // green off, yellow on for 3 seconds
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    delay(3000);
+
+    // turn off yellow, then turn red on for 5 seconds
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+    delay(5000);
+
+    // red and yellow on for 2 seconds (red is already on though)
+    digitalWrite(yellow, HIGH);
+    delay(2000);
+
+    // turn off red and yellow, then turn on green
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+    digitalWrite(green, HIGH);
+    delay(1000); //making sure it fully finishes
+}
